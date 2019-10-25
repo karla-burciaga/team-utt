e8c73ecc168885c100e534728ca90f3759407175
karla burciaga delgado <karla_burciaga_d@outlook.com>
2019-10-25 13:29:27 -0500

    Agregue boostrap

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/karla', function(){
-    return view('karla');
+
+Route::get('/', function () {
+    return view('templates/navbar');
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
+mezclar.pug = require( ' laravel-mix-pug ' );  
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +13,5 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .pug('src/*.pug','dist');
